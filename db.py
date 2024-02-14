7f3a714e2aa4a0fe091bd303e933a730dcf474a9
chunned <72193737+chunned@users.noreply.github.com>
2024-02-14 12:39:48 -0500

    Update db.py

--- db.py
+++ db.py
@@ -20,3 +20,22 @@ def createTables():
   
   # Create ACTOR table
   cur.execute("CREATE TABLE IF NOT EXISTS actor(id INTEGER, name TEXT, country TEXT, PRIMARY KEY (id, name))")
+
+  # RELATIONSHIP TABLES
+  # Studio-Show relationship (Studio produces Show)
+  cur.execute("CREATE TABLE IF NOT EXISTS studio_show(studioID INTEGER, studioName TEXT, showID INTEGER, showName TEXT, FOREIGN KEY(studioID, studioName) REFERENCES studio(id, name), FOREIGN KEY(showID, showName) REFERENCES show(id, name))")
+
+  # Actor-Show relationship A (Actor stars in Show)
+  cur.execute("CREATE TABLE IF NOT EXISTS stars_in()")
+
+  # Actor-Show relationship B (Actor appears in Show)
+  cur.execute("CREATE TABLE IF NOT EXISTS appears_in()")
+
+  # Show-Season relationship (Show contains Season)
+  cur.execute("CREATE TABLE IF NOT EXISTS show_season()")
+  
+  # Season-Episode relationship (Season contains Episode)
+  cur.execute("CREATE TABLE IF NOT EXISTS season_episode()")
+
+  # Genres table 
+  cur.execute("CREATE TABLE IF NOT EXISTS genres(showID INTEGER, showName TEXT, genreName TEXT, )")
